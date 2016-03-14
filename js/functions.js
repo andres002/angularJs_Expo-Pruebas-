4c94f4a6658d7c73363f84d9af6f9777bd985b6b
unknown <aguilarandres296@gmail.com>
2016-03-13 18:31:03 -0600

    avances

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,8 @@ function hola() {
     console.log('entra aqui');
   $('#div-Boton').addClass('cuadro-lista');
   $('#div-Boton').removeClass('boton-lista');
+  $('#todo-list').css('display:absolute;');
+  $('icono').remove();
 
 
 }
